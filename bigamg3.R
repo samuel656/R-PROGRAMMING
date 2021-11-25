964e93771859c206886c9606b65284eb54c07f33
ADIRALA SAMUEL <84395570+samuel656@users.noreply.github.com>
2021-11-25 11:17:31 +0530

    week 2 25-11-2021
    25-11-2021 RLAB

---
+++ bigamg3.R
@@ -0,0 +1,19 @@
+x=readline(prompt("enter first number"))
+a=as.integer(x)
+y=readline(prompt("enter second number"))
+b=as.integer(y)
+z=readline(prompt("enter thhirdf number"))
+c=as.integer(z)
+if(a>b)
+{
+	if(a>c)
+	{
+		print("a is big")
+	}
+}else if(b>c)
+{
+	print("b is big")
+}else
+{
+	print("c is big")
+}
---
+++ vote.R
@@ -0,0 +1,6 @@
+x=readline(prompt("enter your age"))
+age=as.interger(x)
+if(age>=18)
+print("your eli9gible for vote")
+else
+print("not eligible for vote")
